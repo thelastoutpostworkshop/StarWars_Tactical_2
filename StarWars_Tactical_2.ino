06f9da45c3c7a17bb9e04f079f33ca1d2e6bb703
charles <cgiguere@iname.com>
2023-04-09 07:21:04 -0400

    drawMainGrid

--- StarWars_Tactical_2.ino
+++ StarWars_Tactical_2.ino
@@ -4,6 +4,9 @@
 TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
 serialDisplay sDisplay(&tft);
 
+#define COLOR_TACTICAL_DARK_BLUE 0xff
+#define COLOR_TACTICAL_DARK_LIGHTBLUE 0x3eff
+
 void setup()
 {
     Serial.begin(115200);
@@ -17,14 +20,19 @@ void setup()
     tft.fillRect(10, 10, 20, 4, 0x3eff);
     tft.setTextColor(0xff);
     tft.fillRect(10, 70, 40, 4, 0xff);
-    tft.drawFastHLine(0, 85, 128, 0xff);
-    tft.drawFastVLine(15, 0, 90, 0xff);
-    tft.drawFastVLine(55, 0, 90, 0xff);
-    tft.drawFastVLine(95, 0, 90, 0xff);
-    tft.drawFastHLine(0, 5, 128, 0xff);
-    tft.drawFastHLine(15, 90, 5, 0xff);
-    tft.drawFastHLine(55, 90, 5, 0xff);
-    tft.drawFastHLine(95, 90, 5, 0xff);
+    drawMainGrid();
+}
+
+void drawMainGrid()
+{
+    tft.drawFastHLine(0, 85, 128, COLOR_TACTICAL_DARK_BLUE);
+    tft.drawFastVLine(15, 0, 90, COLOR_TACTICAL_DARK_BLUE);
+    tft.drawFastVLine(55, 0, 90, COLOR_TACTICAL_DARK_BLUE);
+    tft.drawFastVLine(95, 0, 90, COLOR_TACTICAL_DARK_BLUE);
+    tft.drawFastHLine(0, 5, 128, COLOR_TACTICAL_DARK_BLUE);
+    tft.drawFastHLine(15, 90, 5, COLOR_TACTICAL_DARK_BLUE);
+    tft.drawFastHLine(55, 90, 5, COLOR_TACTICAL_DARK_BLUE);
+    tft.drawFastHLine(95, 90, 5, COLOR_TACTICAL_DARK_BLUE);
 }
 
 void loop()
