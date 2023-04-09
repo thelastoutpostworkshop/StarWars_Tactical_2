1657474feb98bff76f34b31b0f5689cc6467af53
charles <cgiguere@iname.com>
2023-04-09 08:46:08 -0400

    drawLargeObjects

--- StarWars_Tactical_2.ino
+++ StarWars_Tactical_2.ino
@@ -7,20 +7,37 @@ serialDisplay sDisplay(&tft);
 #define COLOR_TACTICAL_DARK_BLUE 0xff
 #define COLOR_TACTICAL_DARK_LIGHTBLUE 0x3eff
 
+#define BOTTOM_ZONE 90
+#define LARGE_OBJECT_X_SPACING 20
+#define LARGE_OBJECT_Y_SPACING 30
+
 void setup()
 {
     Serial.begin(115200);
-    // Initialize the display
-    tft.init();
-    tft.setRotation(1); // Set screen rotation
 
-    // Fill the screen with a color
+    tft.init();
+    tft.setRotation(1); 
+
     tft.fillScreen(0x0);
-    tft.setTextColor(0x3eff);
-    tft.fillRect(10, 10, 20, 4, 0x3eff);
-    tft.setTextColor(0xff);
-    tft.fillRect(10, 70, 40, 4, 0xff);
     drawMainGrid();
+    drawLargeObjects();
+}
+
+void drawLargeObjects(void)
+{
+    int current_x = 5;
+    int current_y =10;
+    while (current_x < tft.width())
+    {
+        tft.fillRect(current_x, 70, 40, 4, COLOR_TACTICAL_DARK_BLUE);
+        current_x += LARGE_OBJECT_X_SPACING;
+        current_y += LARGE_OBJECT_Y_SPACING;
+        if (current_y >= BOTTOM_ZONE)
+        {
+            current_y = 10;
+        }
+        
+    }
 }
 
 void drawMainGrid()
@@ -28,10 +45,10 @@ void drawMainGrid()
     int start = 15;
     int toggle = 1;
     int mini;
-    tft.drawFastHLine(0, 85, tft.width(), COLOR_TACTICAL_DARK_BLUE);
+    tft.drawFastHLine(0, BOTTOM_ZONE-5, tft.width(), COLOR_TACTICAL_DARK_BLUE);
     for (int i = 0; i < 3; i++)
     {
-        tft.drawFastVLine(start, 0, 90, COLOR_TACTICAL_DARK_BLUE);
+        tft.drawFastVLine(start, 0, BOTTOM_ZONE, COLOR_TACTICAL_DARK_BLUE);
         if (toggle == -1)
         {
             mini = start - 5;
@@ -40,7 +57,7 @@ void drawMainGrid()
         {
             mini = start;
         }
-        tft.drawFastHLine(mini, 90, 5, COLOR_TACTICAL_DARK_BLUE);
+        tft.drawFastHLine(mini, BOTTOM_ZONE, 5, COLOR_TACTICAL_DARK_BLUE);
         tft.drawFastHLine(mini, 0, 5, COLOR_TACTICAL_DARK_BLUE);
 
         start += 40;
