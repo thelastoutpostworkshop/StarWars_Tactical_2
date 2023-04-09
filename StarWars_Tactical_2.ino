7ec9d224a248964d7e4f342f21a20eea7f29196b
charles <cgiguere@iname.com>
2023-04-09 08:52:23 -0400

    drawLargeObjects

--- StarWars_Tactical_2.ino
+++ StarWars_Tactical_2.ino
@@ -8,15 +8,16 @@ serialDisplay sDisplay(&tft);
 #define COLOR_TACTICAL_DARK_LIGHTBLUE 0x3eff
 
 #define BOTTOM_ZONE 90
-#define LARGE_OBJECT_X_SPACING 20
+#define LARGE_OBJECT_X_SPACING 50
 #define LARGE_OBJECT_Y_SPACING 30
 
 void setup()
 {
     Serial.begin(115200);
+    randomSeed(analogRead(0));
 
     tft.init();
-    tft.setRotation(1); 
+    tft.setRotation(1);
 
     tft.fillScreen(0x0);
     drawMainGrid();
@@ -26,17 +27,20 @@ void setup()
 void drawLargeObjects(void)
 {
     int current_x = 5;
-    int current_y =10;
+    int current_y = 10;
+    uint32_t color;
+    int object_size;
     while (current_x < tft.width())
     {
-        tft.fillRect(current_x, 70, 40, 4, COLOR_TACTICAL_DARK_BLUE);
-        current_x += LARGE_OBJECT_X_SPACING;
+        color = ((random(2)) == 0) ? COLOR_TACTICAL_DARK_BLUE : COLOR_TACTICAL_DARK_LIGHTBLUE;
+        object_size = ((random(2)) == 0) ? 20 : 40;
+        tft.fillRect(current_x, current_y, object_size, 4, color);
         current_y += LARGE_OBJECT_Y_SPACING;
         if (current_y >= BOTTOM_ZONE)
         {
             current_y = 10;
+            current_x += LARGE_OBJECT_X_SPACING;
         }
-        
     }
 }
 
@@ -45,7 +49,7 @@ void drawMainGrid()
     int start = 15;
     int toggle = 1;
     int mini;
-    tft.drawFastHLine(0, BOTTOM_ZONE-5, tft.width(), COLOR_TACTICAL_DARK_BLUE);
+    tft.drawFastHLine(0, BOTTOM_ZONE - 5, tft.width(), COLOR_TACTICAL_DARK_BLUE);
     for (int i = 0; i < 3; i++)
     {
         tft.drawFastVLine(start, 0, BOTTOM_ZONE, COLOR_TACTICAL_DARK_BLUE);
